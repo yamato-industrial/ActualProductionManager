de3589c9b7bd212ddeff58a3599fc29dc4793748
HirotoYoshizawa <h.yoshizawa@yamato-industrial.co.jp>
2026-07-28 16:46:24 +0900

    開発環境と本番環境の構成を作成

--- ActualProductionManager/Controllers/HomeController.cs
+++ ActualProductionManager/Controllers/HomeController.cs
@@ -7,14 +7,28 @@ namespace ActualProductionManager.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration, IWebHostEnvironment env)
         {
             _logger = logger;
+            _configuration = configuration;
+            _env = env;
         }
 
         public IActionResult Index()
         {
+            // ŠÂ‹«•Ï”‚ğæ“¾
+            var environmentName = _env.EnvironmentName; // Development or Production
+            var schema = _configuration["Database:Schema"];
+            var connectionString = _configuration["Database:ConnectionString"];
+
+            // ƒrƒ…[‚Éî•ñ‚ğ“n‚·
+            ViewData["EnvironmentName"] = environmentName;
+            ViewData["Schema"] = schema;
+            ViewData["ConnectionString"] = connectionString;
+
             return View();
         }
 
